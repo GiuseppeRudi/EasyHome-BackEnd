60e84b56c181ab4385909eb4cefb9a87e8cb5f63
GiuseppeRudi <giu200383.gr@gmail.com>
2025-02-01 11:42:24 +0100

    merge effettuato con successo dal branch dev . controllare eventuali errori

--- src/main/java/it/unical/demacs/informatica/easyhomebackend/controller/UtenteController.java
+++ src/main/java/it/unical/demacs/informatica/easyhomebackend/controller/UtenteController.java
@@ -11,10 +11,10 @@ import it.unical.demacs.informatica.easyhomebackend.service.IUserService;
 import it.unical.demacs.informatica.easyhomebackend.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
